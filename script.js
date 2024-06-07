337df81f14face3bdb7a33878bcbc2f068608b5c
unknown <roshananand2005@gmail.com>
2024-06-07 09:53:28 +0530

    made the stucture of the typing website

---
+++ script.js
@@ -0,0 +1,15 @@
+const p = document.getElementById("type");
+const wordList = [
+    "apple", "banana", "orange", "grape", "mango",
+    "red", "green", "blue", "yellow", "purple",
+    "happy", "sad", "angry", "excited", "calm",
+    "computer", "phone", "book", "table", "chair",
+    "dog", "cat", "bird", "fish", "snake",
+    "house", "tree", "flower", "mountain", "river"
+];
+p.innerText=wordList.join(" ");
+
+
+document.addEventListener("keydown",(keyPress)=>{
+    
+})

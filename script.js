24edf14b34afc42fecc7c263c79106e56e846e2a
unknown <roshananand2005@gmail.com>
2024-06-08 22:16:19 +0530

    added new features llike counting the second

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 const p = document.getElementById("type");
+const dialogBox = document.getElementById("click");
+const show = document.getElementById("showResult");
 const wordList = [
-    "apple", "banana", "orange", "grape", "mango",
+    "apple", "orange", "grape", "mango",
     "red", "green", "blue", "yellow", "purple",
-    "happy", "sad", "angry", "excited", "calm",
-    "computer", "phone", "book", "table", "chair",
-    "dog", "cat", "bird", "fish", "snake",
-    "house", "tree", "flower", "mountain", "river"
+    "happy", "sad", "angry", "excited", "calm"
 ];
-p.innerText=wordList.join(" ");
+const letter = wordList.join(" ".split(""));
+const addSpanArr = [];
+let count = 0;
+let sec = 0;
 
+for (let i = 0; i < letter.length; i++)
+    addSpanArr[i] = `<span>${letter[i]}</span>`
 
-document.addEventListener("keydown",(keyPress)=>{
-    
+const time = () => {
+    setInterval(() => {
+        sec++;
+    }, 1000);
+}
+
+const result = () => {
+    clearInterval(time)
+    show.innerText = `SCORE : ${sec} second`;
+}
+
+dialogBox.addEventListener("click", () => {
+    document.addEventListener("keydown", (keyPress) => {
+        let input = p.children[count].innerText;
+        if (count == 0) time();
+        else if (count == letter.length-1){
+            p.children[count-1].classList.remove("loc");
+            p.children[count].classList.add("loc");
+            result();
+            return;
+        } 
+        if (keyPress.key == input && count <= letter.length - 1) {
+            p.children[(count==0)?0:count-1].classList.remove("loc");
+            p.children[count++].classList.add("loc");
+        } else p.children[count].style.color = "red";
+
+    });
+
+    dialogBox.close();
 })
+p.innerHTML = addSpanArr.join("");
+p.children[count].classList.add("loc");
+
